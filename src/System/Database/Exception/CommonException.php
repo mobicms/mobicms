92e5ad9fe339a870b79dd95dc0a41a25a5d96320
Oleg Kasyanov <dev@mobicms.net>
2021-04-15 16:07:12 +0400

    Added database exceptions classes

---
+++ src/System/Database/Exception/CommonException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of mobicms/mobicms package
+ *
+ * @see       https://github.com/mobicms/mobicms for the canonical source repository
+ * @license   https://github.com/mobicms/mobicms/blob/develop/LICENSE GPL-3.0
+ * @copyright https://github.com/mobicms/mobicms/blob/develop/README.md
+ */
+
+declare(strict_types=1);
+
+namespace Mobicms\System\Database\Exception;
+
+use PDOException;
+
+class CommonException extends PDOException
+{
+}
