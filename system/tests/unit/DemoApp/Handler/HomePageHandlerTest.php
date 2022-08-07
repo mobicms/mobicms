27b94ce180ae6c88aaa8fb7eb4f0182ded074228
Oleg Kasyanov <oleg@batumi.org>
2022-08-07 20:55:14 +0400

    Updated test

--- system/tests/unit/DemoApp/Handler/HomePageHandlerTest.php
+++ system/tests/unit/DemoApp/Handler/HomePageHandlerTest.php
@@ -6,7 +6,7 @@ namespace MobicmsTest\DemoApp\Handler;
 
 use HttpSoft\Response\HtmlResponse;
 use Mobicms\DemoApp\Handler\HomePageHandler;
-use Mobicms\System\View\Renderer;
+use Mobicms\Render\Engine;
 use Mobicms\Testutils\MysqlTestCase;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -37,7 +37,7 @@ class HomePageHandlerTest extends MysqlTestCase
                 ]
             );
 
-        $renderer = $this->createMock(Renderer::class);
+        $renderer = $this->createMock(Engine::class);
         $renderer
             ->expects($this->once())
             ->method('render')
