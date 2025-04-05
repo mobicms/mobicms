752bf9e582bd64b49eb9a8537a36bdfdecc8b035
Oleg Kasyanov <oleg@batumi.org>
2025-04-05 13:53:49 +0400

    Refactoring

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@ use HttpSoft\ServerRequest\ServerRequestCreator;
 define('START_MEMORY', memory_get_usage());
 define('START_TIME', microtime(true));
 
-chdir(dirname(__DIR__) . '/system');
+chdir(dirname(__DIR__) . '/mobicms');
 require 'vendor/autoload.php';
 
 (function () {
