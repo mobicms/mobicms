776491f35f32749a0c58400f2f040bde89bea570
Oleg Kasyanov <oleg@batumi.org>
2025-04-26 15:36:06 +0400

    Added Docker configuration

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@ use HttpSoft\ServerRequest\ServerRequestCreator;
 define('START_MEMORY', memory_get_usage());
 define('START_TIME', microtime(true));
 
-chdir(dirname(__DIR__) . '/mobicms');
+chdir('../mobicms');
 require 'vendor/autoload.php';
 
 (function () {
